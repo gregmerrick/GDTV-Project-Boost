c3727a619636bdad818ed571fee98cf33b54c834
Greg Merrick <gmerrick86@gmail.com>
2022-04-29 21:30:57 +1000

    Added Oscillating obstacles

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    float movementFactor;
+    [SerializeField] float period = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position; // transform.position is current position.
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (period <= Mathf.Epsilon) { return; } // Comparing two floats is risky, always a chance they won't match, better to use Epsilon.
+        float cycles = Time.time / period; // continually growing over time.
+
+        const float tau = Mathf.PI * 2; // constant value of 6.283 (radian).
+        float rawSinWave = Mathf.Sin(cycles * tau); // going from -1 to 1.
+
+        movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1 so it's cleaner.
+
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPosition + offset;
+    }
+}
